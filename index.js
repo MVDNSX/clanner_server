f575677183fb6bdd3adc687ab81abf864f2b214a
MVDNS <a.s.meleshenkov@gmail.com>
2025-03-29 11:15:40 +0300

    20

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const bot = new TelegramBot(process.env.TOKEN_BOT, {polling: true});
 const port = process.env.PORT || 5000
 
 const app = express()
-app.use(corsMiddleware)
+//app.use(corsMiddleware)
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
