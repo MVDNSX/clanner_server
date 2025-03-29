d0ec392cf8c4b66cba15a0faf479534283459cd7
MVDNS <a.s.meleshenkov@gmail.com>
2025-03-29 11:30:22 +0300

    20

--- middleware/validate.middleware.js
+++ middleware/validate.middleware.js
@@ -5,30 +5,31 @@ const TOKEN = process.env.TOKEN_BOT
 const validateInitData = (req, res, next) => {
 
   const {initData} = req.body
-  if(!initData){
-    return res.status(400).json({message: 'initData отсутствует'})
-  }
+  console.log(initData)
+  //if(!initData){
+  //  return res.status(400).json({message: 'initData отсутствует'})
+  //}
 
-  const params = new URLSearchParams(initData)
-  const hash = params.get('hash')
+  //const params = new URLSearchParams(initData)
+  //const hash = params.get('hash')
 
-  if(!hash){
-    return res.status(400).json({message: 'hash отсутствует'})
-  }
-  params.delete('hash')
+  //if(!hash){
+  //  return res.status(400).json({message: 'hash отсутствует'})
+  //}
+  //params.delete('hash')
 
-  const dataString = [...params.entries()]
-                          .map(([key, value]) => {`${key}=${value}`})
-                          .sort()
-                          .join("\n");
+  //const dataString = [...params.entries()]
+  //                        .map(([key, value]) => {`${key}=${value}`})
+  //                        .sort()
+  //                        .join("\n");
 
-  const secretKey = crypto.createHmac("sha256", 'WebAppData').update(TOKEN).digest()
-  const calculatedHash = crypto.createHmac('sha256', secretKey).update(dataString).digest('hex');
+  //const secretKey = crypto.createHmac("sha256", 'WebAppData').update(TOKEN).digest()
+  //const calculatedHash = crypto.createHmac('sha256', secretKey).update(dataString).digest('hex');
 
-  if(calculatedHash !== hash){
-    return res.status(403).json({ error: "Неверный hash. Данные могут быть подделаны." });
-  }
-  console.log(calculatedHash === hash)
+  //if(calculatedHash !== hash){
+  //  return res.status(403).json({ error: "Неверный hash. Данные могут быть подделаны." });
+  //}
+  //console.log(calculatedHash === hash)
 
   next();
 }
