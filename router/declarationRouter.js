702d1d0d32ff881352b67b759b81081075c869ab
MVDNS <a.s.meleshenkov@gmail.com>
2025-04-28 22:56:30 +0300

    work with methods

--- router/declarationRouter.js
+++ router/declarationRouter.js
@@ -1,8 +1,7 @@
 const Router = require('express')
 const router = new Router()
-const validateMiddleware = require('../middleware/validate.middleware');
 const declarationController = require('../controller/declarationController');
 
-router.post('/create', validateMiddleware, declarationController.processInit)
+router.post('/create', declarationController.processInit)
 
 module.exports = router
