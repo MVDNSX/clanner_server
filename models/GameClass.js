25e1c8af42c39f03db709b2a4a647436c0ea26b7
MVDNS <a.s.meleshenkov@gmail.com>
2025-05-15 23:23:20 +0300

    last change

--- models/GameClass.js
+++ models/GameClass.js
@@ -4,7 +4,7 @@ const sequelize = require('../db')
 //Таблица классов персонажей
 const GameClass = sequelize.define('GameClass', {
  class_name: {type: DataTypes.STRING, allowNull:false, unique:true},
- icon_url: {type: DataTypes.STRING, allowNull:true},
+ icon_url: {type: DataTypes.STRING, allowNull:false},
 }, {
   tableName: 'classes',
   timestamps:false,
