0f0387418ee90f304cdb5088516dec16267fe98d
MVDNS <a.s.meleshenkov@gmail.com>
2025-05-25 12:33:42 +0300

    fix

--- router/memberRouter.js
+++ router/memberRouter.js
@@ -2,7 +2,7 @@ const Router = require('express')
 const router = new Router()
 const memberController = require('../controller/memberController')
 
-router.post('/authMember', memberController.authMember)
+router.post('/auth', memberController.authMember)
 
 router.get('/getAllMember', memberController.getAllMember)
 router.post('/updateRoleMember', memberController.updateRoleMember)
