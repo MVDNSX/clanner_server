6833d63755b34291dcdfa56f467f0053661ed9b1
MVDNS <a.s.meleshenkov@gmail.com>
2025-05-25 22:54:23 +0300

    fix

--- router/partyRouter.js
+++ router/partyRouter.js
@@ -2,10 +2,10 @@ const Router = require('express')
 const router = new Router()
 const partyControllet = require('../controller/partyController')
 
-router.get('/getMemberParty', partyControllet.getMemberParty)
 router.get('/getAllParty', partyControllet.getAllParty)
 router.post('/createParty', partyControllet.createParty)
 router.post('/chooseLeader', partyControllet.chooseLeader)
+router.post('/getMemberParty', partyControllet.getMemberParty)
 
 
 
